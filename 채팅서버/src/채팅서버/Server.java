2a777ea1b3bfdc15bdd0d26e4f9949414054851c
ggavi2000 <ggavi2000@naver.com>
2019-08-02 21:30:17 +0900

    java 채팅-클라이언트 GUI 및 간단 연동

---
+++ 채팅서버/src/채팅서버/Server.java
@@ -0,0 +1,178 @@
+package 채팅서버;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class Server extends JFrame implements ActionListener {
+
+	// [GUI 1] 별개의 프로젝트에서 윈도우 빌더로 만든 뒤 가져옴
+	private JPanel contentPane;
+	private JTextField port_tf;
+	
+	
+	// [GUI 2] 쉽게 쓰기 위해  init() 함수에서 가져와서 전역변수로 바꿈
+	JTextArea textArea = new JTextArea();
+	JButton start_btn = new JButton("서버 실행");
+	JButton stop_btn = new JButton("서버 중지");
+	
+	
+	// [서버 1]
+	private ServerSocket server_socket;
+	private Socket socket;
+	private int port;
+	
+	
+	// 생성자
+	Server()
+	{
+		init();   // 1. 화면 생성 메소드 바로 발동 (GUI 소스코드)
+		start();  // 2. 각 버튼에 대한 액션 리스너 지정
+	}
+
+	
+	// [서버 1] 서버 구축 (사용자가 접속하기를 기다리는 소켓을 생성)
+	private void Server_start()
+	{
+		try {
+			server_socket = new ServerSocket(port);  // 포트번호
+		} 
+		
+		catch (IOException e) {
+			System.out.println("포트가 이미 열려 있으면, 포트를 열 수 없어서 에러가 발생하므로 try-catch 문으로 묶어준다.");
+			e.printStackTrace();
+		} 
+		
+		// 만약 서버 소켓이 정상적으로 열렸을 경우
+		if(server_socket != null)
+		{
+			Connection();  // 사용자가 접속되게 한다.
+		}
+	}
+	
+	// [서버 2] 실질적으로 사용자가 들어오는 부분 작성 (사용자가 접속되게 하기 위한 과정)
+	private void Connection()
+	{
+		// 쓰레드 쓰는 이유: accept() 함수에서 사용자 접속을 대기하는 동안 프로그램은 다른 일을 못하고 죽어버린다.
+		Thread th = new Thread(new Runnable() 
+		{
+			@Override
+			public void run()
+			{
+				try {			
+					// 접속전
+					textArea.append("사용자 접속 대기중 \n");
+					socket = server_socket.accept();  // 사용자 접속 대기 (무한 대기)
+					
+					// 접속후
+					textArea.append("사용자 접속함 \n");
+					
+				} catch (IOException e) {
+					System.out.println("사용자가 접속할 때 에러가 발생할 수 있기 때문에 try-catch 문으로 묶어준다.");
+					e.printStackTrace();
+				}  
+			}
+		});
+		
+		// 쓰레드 실행!
+		th.start();
+	}
+
+	
+	// [GUI 1] 화면 생성 메소드 (GUI 소스코드)
+	private void init()  
+	{
+		// 자바 GUI (윈도우 빌더로 만든 뒤 가져옴)
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 420);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 10, 412, 269);
+		contentPane.add(scrollPane);
+		
+		// JTextArea textArea = new JTextArea();   // 얘는 전역변수로 따로 뺌
+		scrollPane.setViewportView(textArea);
+		
+		JLabel lblNewLabel = new JLabel("포트 번호");
+		lblNewLabel.setBounds(12, 296, 57, 15);
+		contentPane.add(lblNewLabel);
+		
+		port_tf = new JTextField();
+		port_tf.setBounds(81, 293, 341, 21);
+		contentPane.add(port_tf);
+		port_tf.setColumns(10);
+		
+		//JButton btnNewButton = new JButton("서버 실행");  // 전역으로 뺌
+		start_btn.setBounds(12, 342, 200, 23);
+		contentPane.add(start_btn);
+		
+		// JButton btnNewButton_1 = new JButton("서버 중지");  // 전역으로 뺌
+		stop_btn.setBounds(222, 342, 200, 23);
+		contentPane.add(stop_btn);
+		// ↑ 이 윗부분은 전부 윈도우 빌더로 만든 뒤 가져온 소스코드
+				
+		
+		this.setVisible(true);  // true = 화면에 보이게, false = 화면에 보이지 않게  
+	}
+	
+	
+	// [GUI 2] 각 버튼에 대한 액션 리스너 지정
+	private void start() 
+	{
+		// this 쓰는 이유: 이 자체 클래스에서 액션 리스너를 상속받았기 때문에
+		start_btn.addActionListener(this); 
+		stop_btn.addActionListener(this);
+	}
+	
+
+	@Override
+	// [GUI 3] 이벤트 리스너 (인터페이스인 ActionListener를 상속받으면 아래 함수를 재정의 해줘야 한다)
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == start_btn) {
+			System.out.println("디버깅: start 버튼 클릭");
+
+			port = Integer.parseInt(port_tf.getText().trim());  // 포트번호
+			Server_start();  // [서버 1] 함수 실행
+		}
+		
+		else if(e.getSource() == stop_btn) {
+			System.out.println("디버깅: 서버 stop 버튼 클릭");
+		}
+	}
+	
+	// 이벤트를 구분하는 방법 2가지 
+	// 1. 버튼의 이름으로 구별
+	// 2. 바로 직접 비교하는 방법
+	
+	
+	public static void main(String[] args) {
+		new Server();  // 익명으로 객체 생성
+	}
+
+	
+
+	
+	/*
+		이벤트 리스너 쓰는법
+	
+		1. 직접 상속
+		2. 익명클래스로 작성
+		3. 내부클래스, 외부클래스
+	*/
+}
---
+++ 채팅서버/src/채팅클라이언트/Client.java
@@ -0,0 +1,248 @@
+package 채팅클라이언트;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+// 프레임을 만드는 2가지 방법: 직접 JFrame을 상속받아서 만들 수도 있고, 객체를 발생시키는 방법도 있다.
+public class Client extends JFrame implements ActionListener {		
+
+	// [GUI 1] 로그인 GUI 변수 (별개의 프로젝트에서 윈도우 빌더로 만든 뒤 가져옴)
+	private JFrame Login_GUI = new JFrame();  // 로그인 창은 따로 발생시킬 것이다.
+	private JPanel Login_Pane;
+	private JTextField ip_tf;    // ip를 받는 텍스트 필드
+	private JTextField port_tf;  // port를 받는 텍스트 필드
+	private JTextField id_tf;    // id를 받는 텍스트 필드
+	JButton login_btn = new JButton("접속하기");  // 버튼 (필요한 자원들을 전역변수로 뺌)
+	
+	
+	// [GUI 2] Main GUI 변수 (이것도 윈도우 빌더로 만든 후 가져옴) 
+	private JPanel contentPane;
+	private JTextField message_tf;
+	private JButton notesend_btn = new JButton("쪽지보내기");   // 버튼 (필요한 자원들을 전역변수로 뺌)
+	private JButton joinroom_btn = new JButton("채팅방 참여");  // 버튼 (필요한 자원들을 전역변수로 뺌)
+	private JButton createroom_btn = new JButton("방 만들기"); // 버튼 (필요한 자원들을 전역변수로 뺌)
+	private JButton send_btn = new JButton("전송");          // 버튼 (필요한 자원들을 전역변수로 뺌)
+	
+	private JList User_list = new JList();  // 전체 접속자 List
+	private JList Room_list = new JList();  // 전체 방 목록 List
+	JTextArea Chat_area = new JTextArea();   // 채팅을 할 때 창 부분
+	
+	
+	// [서버 1] 네트워크를 위한 자원 변수
+	private Socket socket;  // 클라이언트 소켓
+	private String ip; // = "127.0.0.1";  // 127.0.0.1은 자기 자신
+	private int port; // = 12345;
+	
+	
+	// 생성자
+	Client() 
+	{
+		Login_init();  // 1. 로그인 창 화면 구성
+		Main_init();   // 2. Main 클라이언트 창 화면
+		start(); 	   // 3. 버튼에 대한 액션 리스너 지정
+	}
+	
+	
+	
+	// [서버 1]
+	private void Network()  // try-catch 쓰는 이유: 사용자가 접속할 때 에러가 날 수도 있으므롤 
+	{
+		try {
+			socket = new Socket(ip, port);
+		} 
+		
+		catch (UnknownHostException e) {
+			System.out.println("해당 호스트를 찾을 수 없을 때 발생하는 에러");
+			e.printStackTrace();
+		} 
+		
+		catch (IOException e) {
+			System.out.println("스트림에서 발생하는 에러");
+			e.printStackTrace();
+		}
+	}
+	
+	
+	// [GUI 1] 로그인 화면 띄우기 (윈도우 빌더에서 만든 뒤 가져와서 복붙)
+	private void Login_init()
+	{
+		// ↓ 수정     (로그인 창은 별도로 생성시킬 것이므로 앞부분에 Login_GUI.을 붙였다)
+		Login_GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+		Login_GUI.setBounds(100, 100, 270, 285);
+		Login_Pane = new JPanel();
+		Login_Pane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		Login_GUI.setContentPane(Login_Pane);
+		Login_Pane.setLayout(null);
+		// ↑ JFrame을 상속받았기 때문에 본래 앞에 this.가 생략되어 있었는데 Login_GUI.으로 수정
+		
+		JLabel lblNewLabel = new JLabel("Server IP");
+		lblNewLabel.setBounds(25, 30, 57, 15);
+		Login_Pane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Server Port");
+		lblNewLabel_1.setBounds(25, 80, 73, 15);
+		Login_Pane.add(lblNewLabel_1);
+		
+		JLabel lblId = new JLabel("ID");
+		lblId.setBounds(25, 130, 57, 15);
+		Login_Pane.add(lblId);
+		
+		ip_tf = new JTextField();
+		ip_tf.setBounds(107, 30, 116, 21);
+		Login_Pane.add(ip_tf);
+		ip_tf.setColumns(10);
+		
+		port_tf = new JTextField();
+		port_tf.setBounds(107, 80, 116, 21);
+		Login_Pane.add(port_tf);
+		port_tf.setColumns(10);
+		
+		id_tf = new JTextField();
+		id_tf.setBounds(107, 130, 116, 21);
+		Login_Pane.add(id_tf);
+		id_tf.setColumns(10);
+		
+		// JButton btnNewButton = new JButton("접속하기");
+		login_btn.setBounds(25, 200, 198, 23);
+		Login_Pane.add(login_btn);
+		// ↑ 여기까지가 윈도우 빌더로 만든 뒤 가져온 소스코드 
+		
+		
+		Login_GUI.setVisible(true);  // true: 화면에 보이게, false: 화면에 보이지 않게
+	}
+	
+	
+
+	// [GUI 2] Main 클라이언트 화면
+	private void Main_init()
+	{
+		// 자바 GUI (윈도우 빌더에서 만든 뒤 가져와서 복붙)
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // JFrame을 상속받았기 때문에 앞에 this.가 생략되어 있음
+		setBounds(100, 100, 553, 460);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("접속자");
+		lblNewLabel.setBounds(46, 21, 45, 15);
+		contentPane.add(lblNewLabel);
+		
+		// JList list = new JList();  // 전역으로 뺌
+		User_list.setBounds(12, 46, 105, 111);
+		contentPane.add(User_list);
+		
+		// JButton button = new JButton("쪽지보내기"); // 전역으로 뺌
+		notesend_btn.setBounds(12, 163, 105, 23);
+		contentPane.add(notesend_btn);
+		
+		JLabel label = new JLabel("채팅방 목록");
+		label.setBounds(32, 202, 85, 15);
+		contentPane.add(label);
+		
+		// JList list_1 = new JList();  // 전역으로 뺌
+		Room_list.setBounds(12, 227, 105, 120);
+		contentPane.add(Room_list);
+		
+		// JButton btnNewButton = new JButton("채팅방 참여");  // 전역으로 뺌
+		joinroom_btn.setBounds(12, 357, 105, 23);
+		contentPane.add(joinroom_btn);
+		
+		// JButton btnNewButton_1 = new JButton("방 만들기");  // 전역으로 뺌
+		createroom_btn.setBounds(12, 388, 105, 23);
+		contentPane.add(createroom_btn);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(129, 46, 404, 338);
+		contentPane.add(scrollPane);
+		
+		// JTextArea textArea = new JTextArea();  // 채팅을 할 때 창 부분 (전역으로 뺌)
+		scrollPane.setViewportView(Chat_area);
+		
+		message_tf = new JTextField();
+		message_tf.setBounds(129, 389, 295, 21);
+		contentPane.add(message_tf);
+		message_tf.setColumns(10);
+		
+		// JButton btnNewButton_2 = new JButton("전송");   // 전역으로 뺌
+		send_btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		send_btn.setBounds(428, 388, 105, 23);
+		contentPane.add(send_btn);
+		
+		this.setVisible(true);  // true: 화면 보이게, false: 화면 안보이게
+	}
+	
+	
+	
+	// [GUI 3] 버튼에 대한 액션 리스너 지정
+	private void start()
+	{
+		// this 쓰는 이유: 이 자체 클래스에서 액션 리스너를 상속받았기 때문에
+		login_btn.addActionListener(this);       // 로그인 버튼 리스너
+		notesend_btn.addActionListener(this);    // 쪽지보내기 버튼 리스너
+		joinroom_btn.addActionListener(this);    // 채팅방 참여 버튼 리스너
+		createroom_btn.addActionListener(this);  // 채팅방 만들기 버튼 리스너
+		send_btn.addActionListener(this);        // 채팅 전송 버튼 리스너
+	}
+	
+	
+	
+	@Override
+	// [GUI 4] 이벤트 리스너: 인터페이스인 ActionListener를 상속받으면 아래 함수를 재정의 해줘야 한다.
+	public void actionPerformed(ActionEvent e) {
+
+		if(e.getSource() == login_btn)
+		{
+			System.out.println("디버깅: 로그인 버튼 클릭");
+			Network(); // [서버 1] 함수 실행
+			
+			// 입력창 3개
+			ip = ip_tf.getText().trim();
+			port = Integer.parseInt(port_tf.getText().trim());
+			
+		}
+		
+		else if(e.getSource() == notesend_btn)
+		{
+			System.out.println("디버깅: 쪽지 보내기 버튼 클릭");
+		}
+		
+		else if(e.getSource() == joinroom_btn)
+		{
+			System.out.println("디버깅: 방 참여 버튼 클릭");
+		}
+		
+		else if(e.getSource() == createroom_btn)
+		{
+			System.out.println("디버깅: 방 만들기 버튼 클릭");
+		}
+		
+		else if(e.getSource() == send_btn)
+		{
+			System.out.println("디버깅: 채팅 전송 버튼 클릭");
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		new Client();  // 서버 만들 때와 똑같이 익명 함수로 생성
+	}
+}
